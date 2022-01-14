62cd49fb6a0bb9e615cdef7d142a025d4f2c1183
Bompally Harish <harishfebblar@gmail.com>
2022-01-14 14:35:38 +0530

    added default page

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.get('/',(req,res)=>{
+    res.send('Welcome to the first project using terminal github')
+})
+
 const port = 3000;
 app.listen(port, ()=>{
 console.log('server started at port: '+ port)
